759e8a591f7e221adaa2a7e8b355c37eaa2d61e5
Big Boss <thya9o@outlook.com>
2018-09-07 13:37:11 -0300

    plugin default config

--- web/app/js/main.js
+++ web/app/js/main.js
@@ -1,5 +1,28 @@
+// eventos globais
 $(document).ready(function() {
+	// incializa os plugins
 	$(function () {
+		// inicializa o confirm
+		jconfirm.defaults = {
+			title: '<b class="text-danger"><i class="fa fa-exclamation-triangle"></i>&nbsp; Atenção<b>',
+			typeAnimated: true,
+			draggable: true,
+			content: '',
+			buttons: {},
+			defaultButtons: {},
+			buttons: {
+				ok: { 
+					text: '<i class="fa fa-check fa-fw"></i>',
+					btnClass: 'btn-primary btn-flat',
+				},
+				close: { 
+					text: '<i class="fa fa-times fa-fw"></i>',
+					btnClass: 'btn-danger btn-flat',
+				},
+			},
+		};
+		
+		// inicializa o tooltip
 		  $('[data-toggle="tooltip"]').tooltip();
 	});
 });
