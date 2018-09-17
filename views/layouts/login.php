f57b453a7d9499ec6036cc130d335c7980c21911
Big Boss <thya9o@outlook.com>
2018-09-17 00:21:15 -0300

    fix login icon

--- views/layouts/login.php
+++ views/layouts/login.php
@@ -11,11 +11,9 @@ AppAsset::register($this);
             <meta charset="<?= \Yii::$app->charset; ?>">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <?= Html::csrfMetaTags(); ?>
+            <title><?= \Yii::$app->name.' | '.Html::encode($this->title); ?></title>
             <link rel="shortcut icon" href="<?= \Yii::$app->request->baseUrl; ?>/img/favicon-1.png" type="image/png" />
             <!-- ./favicon -->
-            <title><?= \Yii::$app->name.' | '.Html::encode($this->title); ?></title>
-            <link rel="icon" href="<?= \Yii::getAlias('@web'); ?>/img/favicon.ico?v=3.0.0" type="image/x-icon" />
-			<link rel="shortcut icon" href="<?= \Yii::getAlias('@web'); ?>/img/favicon.ico?v=3.0.0" type="image/x-icon" />
             <?php $this->head(); ?>
         </head>
         <!-- ./head -->
